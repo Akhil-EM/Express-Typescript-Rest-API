b74c5f40facdfe1c470e2f2b5b263f1e632dafe4
Akhil-EM <akhilemcalicut@gmail.com>
2023-10-31 09:53:49 +0530

    removed dist folder

--- dist/validators/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-const user_validator_1 = __importDefault(require("./validator/user.validator"));
-module.exports = {
-    userValidator: user_validator_1.default,
-};
